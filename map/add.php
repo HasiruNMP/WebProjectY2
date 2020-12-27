9c63c6964d4c6dfad2c0662b4b42f0379ecef98f
Rakshitha <rakshithadilshan1@gmail.com>
2020-12-27 15:04:40 +0530

    f

--- map/add.php
+++ map/add.php
@@ -7,11 +7,14 @@ $dbname = "dD4aW06XoB";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if(mysqli_connect_errno())
+{
+	echo mysqli_connect_errno();
+	die();
 }
-echo "Connected successfully";
+
+
+
 
 $firstname = $_POST["fname"];
 $lastname = $_POST["lname"];
@@ -28,5 +31,22 @@ $path = "images/";
 move_uploaded_file($temp_name,$path.$img);
 
 echo  "<br>" . $firstname . "<br>". $lastname . "<br>" . $email . "<br>" . $cropname . "<br>". $croptype . "<br>" . $quantity ."<br>" . $descrip .  "<br>"  . $latitude . "<br>"   . $longitude . "<br>". $img . "<br>";
+
+
+if(isset($_REQUEST["submit"]))
+{
+
+
+$query="INSERT INTO reports (email, fname, lname, crop_name, crop_type, photos, lat, longt, description) VALUES ('email', 'firstname', 'lastname', 'cropname', '$croptype', 'img', '5.55', '4.6464', 'descrip');";
+}
+
+ if($conn->query($query))
+ {
+ 	echo "<h1><center>Your order has been sucessfully recorded !</center></h1>";
+ }
+ else
+ 	echo "error";
+
+
 
 ?>
