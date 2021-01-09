4c5dc403192df19fc9c53c8ba29704eb9a8362bb
Rakshitha <rakshithadilshan1@gmail.com>
2021-01-09 14:29:33 +0530

    report

---
+++ map/staff report map.php
@@ -0,0 +1,184 @@
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Marker Animations</title>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAWH-XTux9pCrmqDoV6YM63Ex8FPrAQNLU&callback=initMap&libraries=&v=weekly"
+      defer
+    ></script>
+    <style type="text/css">
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 80%; width:70%;
+        float:right;
+        
+      }
+
+      #leftpanel{
+        height:80%;width:30%;
+        background-color: lightblue;
+        float:left;
+      }
+
+      #reportphoto{
+      	height:100px;width:100px;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html,
+      body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      table {
+  width: 100%;
+}
+
+
+  
+  
+      
+    </style>
+    <script>
+      // The following example creates a marker in Stockholm, Sweden using a DROP
+      // animation. Clicking on the marker will toggle the animation between a BOUNCE
+      // animation and no animation.
+    function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {
+      lat: 7.2906,
+      lng: 80.6337
+    },
+    mapTypeId: 'roadmap'
+  });
+
+
+
+ google.maps.event.addDomListener(document.getElementById('submit'), 'click', function(evt) {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: parseFloat(document.getElementById('lat').value),
+        lng: parseFloat(document.getElementById('lng').value)
+      },
+      map: map
+    });
+  });
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
+  
+
+  
+
+            
+       
+}
+
+
+
+
+
+    </script>
+  </head>
+  <body>
+
+    <div id ="leftpanel"> 
+
+    
+    	<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dD4aW06XoB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn)
+{
+	 echo "Server Not connected";
+}
+else
+{
+	echo "Server connected";
+}
+
+if(!mysqli_select_db($conn,$dbname))
+{
+	echo "Database Not Selected";
+}
+
+else
+{
+	echo "Database Selected";
+}
+
+
+?>
+
+<?php
+
+$result = mysqli_query($conn,"SELECT * FROM reports");
+?>
+
+
+
+
+
+    	<?php
+if (mysqli_num_rows($result) > 0) {
+?>
+  <table border="1">
+  
+<?php
+
+while($row = mysqli_fetch_array($result)) {
+?>
+
+
+
+
+<tr>
+    <td><?php echo $row["report_id"]; ?> <br>
+  		 <u><b id="submit"><?php echo $row["crop_name"]; ?> </b></u><br>
+  		 <?php echo $row["crop_type"]; ?> <br>
+  		 <?php echo $row["description"]; ?> <br>
+   		 <?php echo $row["fname"]; ?>
+    	<?php echo $row["lname"]; ?> <br>
+    	<?php echo $row["email"]; ?><br>
+    	<img id="reportphoto" src="images\<?php echo $row["photos"]; ?>">
+
+    	<input id="lat"  type="hidden" value="<?php echo $row["lat"]; ?>" />
+    	<input id="lng" type="hidden" value="<?php echo $row["longt"]; ?>" />
+    	
+
+    	
+
+    	
+
+
+    </td>
+</tr>
+
+<?php
+}
+?>
+</table>
+ <?php
+}
+else{
+    echo "No result found";
+}
+?>
+
+    </div>
+    <div id="map"></div>
+  </body>
+</html>
+
+
